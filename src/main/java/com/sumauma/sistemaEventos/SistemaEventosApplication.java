26efdd07414a5dc6d10f242ca8ba7573233f15c2
Aristóteles Ferrão <afneto1960@gmail.com>
2024-12-12 17:01:11 -0300

    criação das classes do projeto e respectivas assciações as tabelas

--- src/main/java/com/sumauma/sistemaEventos/SistemaEventosApplication.java
+++ src/main/java/com/sumauma/sistemaEventos/SistemaEventosApplication.java
@@ -1,13 +1,21 @@
 package com.sumauma.sistemaEventos;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SistemaEventosApplication {
+public class SistemaEventosApplication implements CommandLineRunner{
 
 	public static void main(String[] args) {
 		SpringApplication.run(SistemaEventosApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		
+		System.out.println("Banco teste h2 em execução");
+		
 	}
 
 }
