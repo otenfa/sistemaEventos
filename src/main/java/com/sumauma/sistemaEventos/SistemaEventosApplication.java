07360572d1e43177396ca05bc7169883fb630f5d
Aristóteles Ferrão <afneto1960@gmail.com>
2024-12-12 16:26:09 -0300

    criação do projeto, ajustes nos arquivos pom e properties, testes de acesso ao banco de teste

---
+++ src/main/java/com/sumauma/sistemaEventos/SistemaEventosApplication.java
@@ -0,0 +1,13 @@
+package com.sumauma.sistemaEventos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SistemaEventosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SistemaEventosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sumauma/sistemaEventos/SistemaEventosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sumauma.sistemaEventos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SistemaEventosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
